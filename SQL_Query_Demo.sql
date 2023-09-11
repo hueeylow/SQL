f37727041b06e41d5d738618d1631c67cdeb754a
Jia Hui (Singapore) <142574194+hueeylow@users.noreply.github.com>
2023-09-11 08:07:22 +0800

    Update SQL_Query_Demo.sql

--- SQL_Query_Demo.sql
+++ SQL_Query_Demo.sql
@@ -29,8 +29,8 @@ FROM HRDataDB.dbo.HRData as a
 		ON a.EmployeeNumber = b.EmpID
 WHERE YearsAtCompany > = 20
 
--- Query 4 Statement: Show list of employees ID, annual based salary, new based salary based on percent increment hike, job title and department, whose employees’ job level is 5.
-SELECT EmployeeNumber, MonthlyIncome*12 as AnnualBaseSalary, (MonthlyIncome*(PercentSalaryHike/100))+MonthlyIncome as NewAnnualBasedSalary, JobRole, Department
+-- Query 4 Statement: Show list of employees ID, annual base salary, new base salary based on percent salary hike, job title and department, whose employeesâ€™ job level is 5.
+SELECT EmployeeNumber, MonthlyIncome*12 as AnnualBaseSalary, (MonthlyIncome*(PercentSalaryHike/100))*12 as NewAnnualBasedSalary, PercentSalaryHike, JobRole, Department
 FROM HRDataDB.dbo.HRData as a
 	FULL JOIN HRDataDB.dbo.EmpSalary as b
 		ON a.EmployeeNumber = b.EmpID
@@ -54,7 +54,7 @@ WHERE MonthlyIncome >5000
 GROUP BY JobRole
 
 
--- Query 7 Statement: Show top 5 Employee ID by monthly income, job role, department where monthly income is less than 5000 and training times last year is at least 6 years
+-- Query 7 Statement: Show top 5 Employee ID by monthly income, job role, department where monthly income is less than $5,000 and training times last year is at least 6 years
 SELECT Top(5) EmployeeNumber,  MonthlyIncome, JobRole, Department
 FROM HRDataDB.dbo.HRData as a
 	FULL JOIN HRDataDB.dbo.EmpSalary as b
@@ -62,4 +62,3 @@ FROM HRDataDB.dbo.HRData as a
 WHERE MonthlyIncome <5000
 AND TrainingTimesLastYear > = 6
 ORDER BY MonthlyIncome DESC
-
